88c84407b2ef7b75345a3552a1ab45399d8bb718
JWN <naa3000@naver.com>
2025-02-15 17:29:57 +0900

    first commit

---
+++ src/main/java/jwn/linux_in_minecraft/LinuxInMinecraftClient.java
@@ -0,0 +1,12 @@
+package jwn.linux_in_minecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LinuxInMinecraftClient implements ClientModInitializer {
+    public static final String MOD_ID = "linux_in_minecraft";
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
